a9b9f73eea9dbfee8d69bafcd9f8b5dcb4bb4943
masterofcsharp <109514731+masterofcsharp@users.noreply.github.com>
2023-06-28 10:10:15 +0000

    ex 2

--- part 2/exercices 2.1 - 2.5/Course.jsx
+++ part 2/exercices 2.1 - 2.5/Course.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
 const Course = ({ course }) => {
+  const totalExercises = course.parts.reduce((sum, part) => sum + part.exercises, 0);
+
   return (
     <div>
       <Header courseName={course.name} />
       <Content parts={course.parts} />
+      <TotalExercises total={totalExercises} />
     </div>
   );
 };
@@ -30,6 +33,12 @@ const Part = ({ part }) => {
     <p>
       {part.name} - {part.exercises} exercises
     </p>
+  );
+};
+
+const TotalExercises = ({ total }) => {
+  return (
+    <p>Total number of exercises: {total}</p>
   );
 };
 
