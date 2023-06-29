7a46f3dd2eb65dfe64829337170f7cdd7664ee08
masterofcsharp <109514731+masterofcsharp@users.noreply.github.com>
2023-06-29 07:48:04 +0000

    phonebook

--- part 2/exercices 2.6 - 2.10/App.jsx
+++ part 2/exercices 2.6 - 2.10/App.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import Filter from './Filter';
+import PersonForm from './PersonForm';
+import Persons from './Persons';
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -52,26 +55,22 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        Filter by name: <input value={searchTerm} onChange={handleSearchChange} />
-      </div>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      {filteredPersons.map((person) => (
-        <div key={person.id}>
-          {person.name} {person.number}
-        </div>
-      ))}
+
+      <Filter searchTerm={searchTerm} onSearchChange={handleSearchChange} />
+
+      <h3>Add a new</h3>
+
+      <PersonForm
+        newName={newName}
+        newNumber={newNumber}
+        onNameChange={handleNameChange}
+        onNumberChange={handleNumberChange}
+        onSubmit={addPerson}
+      />
+
+      <h3>Numbers</h3>
+
+      <Persons persons={filteredPersons} />
     </div>
   );
 };
