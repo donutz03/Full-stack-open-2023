611b2f736c6e9c0c4a15f1b446aa4162274018a7
masterofcsharp <109514731+masterofcsharp@users.noreply.github.com>
2023-07-01 19:17:24 +0300

    Create Filter.js

---
+++ part 2/exercice 2.11/Filter.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Filter = ({ searchTerm, onSearchChange }) => {
+  return (
+    <div>
+      filter shown with: <input value={searchTerm} onChange={onSearchChange} />
+    </div>
+  );
+};
+
+export default Filter;
