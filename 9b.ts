10d1cce63fb36486db03e46ecfbcc1b7085773ac
donutz03 <109514731+donutz03@users.noreply.github.com>
2023-08-01 17:12:06 +0300

    Update 9b.ts

--- 9b.ts
+++ 9b.ts
@@ -3,3 +3,12 @@ try {
  } catch (error) {
 console.log('smt went wrong' + error.message);
 }
+try {
+  console.log(calculator(1, 5 , 'divide'));
+} catch (error: unknown) {
+  let errorMessage = 'Something went wrong: '
+  if (error instanceof Error) {
+    errorMessage += error.message;
+  }
+  console.log(errorMessage);
+}
