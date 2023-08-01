799df75a6cddb27a6b8d9c1ab23262cee7a07dd6
donutz03 <109514731+donutz03@users.noreply.github.com>
2023-08-01 12:43:37 +0300

    Update 9a.ts

--- 9a.ts
+++ 9a.ts
@@ -51,7 +51,16 @@ const multiplicator = (a: number, b: number, printText: string) => {
 multiplicator('str', 4, 'str cu 4 nu mere')
 type Operation = 'multiply' | 'add' | 'divide';
 
-
+const calculator = (a: number, b: number, op: Operation) => {
+if (op === 'multiply') {
+  return a * b;
+} else if (op === 'add') {
+return a + b;
+} else if (op === 'divide') {
+  if (b===0) return 'can\'t divide by 0!';
+  return a / b;
+  }
+}
 
 
 
