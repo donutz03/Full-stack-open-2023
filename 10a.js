79ff6410ada342a31048f36c64e58e873877c68c
donutz03 <109514731+donutz03@users.noreply.github.com>
2023-08-03 16:56:30 +0300

    Update 10a.js

--- 10a.js
+++ 10a.js
@@ -1,3 +1,5 @@
 
 npx create-expo-app rate-repository-app --template expo-template-blank@sdk-46
 {"scripts":{"start": "expo start","android":"expo start --android","ios":"expo start --ios","web":"expo start --web"},}
+{"scripts":{"start": "expo start","android":"expo start --android","ios":"expo start --ios","web":"expo start --web"},}
+{"plugins":["react","react-native"],"settings":{"react":{"version":"detect"}},"extends":["eslint:"recommended"],"parser":"@babel/eslint-parser","env":{"react-native/react-native": true},"rules":{"react/prop-types":"off","react/react-in-jsx-scope":"off"}}
