87ccb6f91f0757c160e3f98f1c562954cbb6f842
donutz03 <109514731+donutz03@users.noreply.github.com>
2023-08-03 16:49:00 +0300

    Update 10a.js

--- 10a.js
+++ 10a.js
@@ -1,2 +1,3 @@
 
 npx create-expo-app rate-repository-app --template expo-template-blank@sdk-46
+{"scripts":{"start": "expo start","android":"expo start --android","ios":"expo start --ios","web":"expo start --web"},}
